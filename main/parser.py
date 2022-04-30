4f85d4087a393120e2737bca793a336c5f2f4d26
slash <slash526@gmail.com>
2022-04-30 17:09:50 +0300

    Parser feature

--- main/parser.py
+++ main/parser.py
@@ -8,7 +8,15 @@ class Parser:
         text = r.text
         self.image = re.findall(r"https://st\..*\.jpg", text)[0]
         self.name = re.findall(r"<h1>.*</h1>", text)[0][4:-5]
-        self.description = re.findall(r'"description">.*</div>', text)[0][14:-6]
+        try:
+            self.description = re.findall(r'"description">.*</div>', text)[0][14:-6]
+        except IndexError as e:
+            self.description = re.findall(r'"description">.*<br', text)[0][14:-3]
 
     def response(self):
         return [self.name, self.description, self.image]
+
+
+if __name__ == "__main__":
+    x = Parser("https://kinobase.org/film/4923-virus")
+    print(x.response())
